c21ee6d62760e0641ca04282cc5237b75062db16
Ryan Niño Dizon <ryan.dizon@isdfeniqs.com>
2024-05-06 00:50:09 +0200

    Added images

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {RouterLink, RouterOutlet} from '@angular/router';
     <main>
       <a [routerLink]="['/']">
         <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+          <img class="brand-logo" src="/assets/abp-logo.JPG" alt="logo" />
         </header>
       </a>
       <section class="content">
